a5b98932949cd09f70efca85c310736aeb9c39da
Rohitt Shinde <shinde.rohitt@gmail.com>
2014-03-12 22:18:25 +0530

    add pages_likes table

--- setup/_create_database.sql
+++ setup/_create_database.sql
@@ -267,3 +267,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   KEY `emailAddress_2` (`emailAddress`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
+--
+-- Table structure for table `pages_dislikes`
+--
+
+CREATE TABLE IF NOT EXISTS `pages_dislikes` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int(11) NOT NULL,
+  `pageID` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
